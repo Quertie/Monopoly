98343cee1563ae8262f8e8d3d2c59547948226c3
Thomas Drake <thomas.drake@live.fr>
2023-01-13 01:10:40 +0100

    Changed properties to auto-properties

--- Assets/Scripts/Boards/Classic/Style/MonopolyClassicTheme.cs
+++ Assets/Scripts/Boards/Classic/Style/MonopolyClassicTheme.cs
@@ -14,16 +14,16 @@ namespace Boards.Classic.Style
         public const int CornerNameFontSize = 40;
         public const int GoFontSize = 65;
 
-        public static System.Drawing.Color LightGreen => System.Drawing.Color.FromArgb(191, 219, 174);
-        public static System.Drawing.Color Black => System.Drawing.Color.Black;
-        public static System.Drawing.Color BrownGroupColor => System.Drawing.Color.FromArgb(132, 77, 36);
-        public static System.Drawing.Color LightBlueGroupColor => System.Drawing.Color.FromArgb(154, 207, 239);
-        public static System.Drawing.Color VioletGroupColor => System.Drawing.Color.FromArgb(206, 37, 128);
-        public static System.Drawing.Color OrangeGroupColor => System.Drawing.Color.FromArgb(236, 154, 19);
-        public static System.Drawing.Color RedGroupColor => System.Drawing.Color.FromArgb(218, 10, 44);
-        public static System.Drawing.Color YellowGroupColor => System.Drawing.Color.FromArgb(230, 226, 65);
-        public static System.Drawing.Color GreenGroupColor => System.Drawing.Color.FromArgb(41, 170, 90);
-        public static System.Drawing.Color DarkBlueGroupColor => System.Drawing.Color.FromArgb(58, 90, 151);
+        public static System.Drawing.Color LightGreen { get; } = System.Drawing.Color.FromArgb(191, 219, 174);
+        public static System.Drawing.Color Black { get; } = System.Drawing.Color.Black;
+        public static System.Drawing.Color BrownGroupColor { get; } = System.Drawing.Color.FromArgb(132, 77, 36);
+        public static System.Drawing.Color LightBlueGroupColor { get; } = System.Drawing.Color.FromArgb(154, 207, 239);
+        public static System.Drawing.Color VioletGroupColor { get; } = System.Drawing.Color.FromArgb(206, 37, 128);
+        public static System.Drawing.Color OrangeGroupColor { get; } = System.Drawing.Color.FromArgb(236, 154, 19);
+        public static System.Drawing.Color RedGroupColor { get; } = System.Drawing.Color.FromArgb(218, 10, 44);
+        public static System.Drawing.Color YellowGroupColor { get; } = System.Drawing.Color.FromArgb(230, 226, 65);
+        public static System.Drawing.Color GreenGroupColor { get; } = System.Drawing.Color.FromArgb(41, 170, 90);
+        public static System.Drawing.Color DarkBlueGroupColor { get; } = System.Drawing.Color.FromArgb(58, 90, 151);
 
         public static FontFamily DeedNameFontFamily{
             get
