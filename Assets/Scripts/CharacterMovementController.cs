6652ae8376c775da22ea721edad9605b32311f12
Quertie <48120978+Quertie@users.noreply.github.com>
2023-08-27 15:55:50 +0200

    Feat/progressively move (#3)
    * feat (progressively-move) Working version, may need improvement in code
    
    * feat(progressively-move) change static camera position
    
    * feat (progressively-move) hide dice roll provider while moving token
    
    * feat (progressively-move) removed unnecessary logs

--- Assets/Scripts/CharacterMovementController.cs
+++ Assets/Scripts/CharacterMovementController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Linq;
 using System.Threading.Tasks;
 using Squares;
@@ -6,33 +7,64 @@ using UnityEngine;
 internal class CharacterMovementController : ICharacterMovementController
 {
     private readonly IGameBoard _gameBoard;
+    private bool _coroutineFinished;
+    private TaskCompletionSource<bool> _coroutineFinishedTaskCompletionSource = new TaskCompletionSource<bool>();
 
-    private static readonly TaskCompletionSource<bool> TaskCompletionSource = new TaskCompletionSource<bool>();
-    
     public CharacterMovementController(IGameBoard gameBoard)
     {
         _gameBoard = gameBoard;
     }
-    
-    public Task MoveToSquare(Square square)
+
+    public async Task MoveToSquare(Square square)
     {
-        var squareIndex = _gameBoard.GetSquareIndex(square);
-        var squareGameObjectName = string.Format(Constants.GameObjectNames.Square, squareIndex);
-        var tokenPositionMarkerGameObjectName = Constants.GameObjectNames.TokenPosition10;
-        void MoveAction()
+        var waypoint = _gameBoard.GetWaypoint(_gameBoard.CurrentSquare, square);
+        foreach (Square step in waypoint)
         {
-            var destinationSquareTransform = GetGameBoardGameObject().GetComponentsInChildren<Transform>().Single(c => c.gameObject.name == squareGameObjectName);
-            var destinationPosition = destinationSquareTransform
-                .GetComponentsInChildren<Transform>().Single(c => c.gameObject.name == tokenPositionMarkerGameObjectName).position;
-            GetPlayerTokenGameObject().transform.position = destinationPosition;
+            var squareIndex = _gameBoard.GetSquareIndex(step);                                      
+            var squareGameObjectName = string.Format(Constants.GameObjectNames.Square, squareIndex);   
+            
+            var tokenPositionMarkerGameObjectName = Constants.GameObjectNames.TokenPosition10;
+
+            _coroutineFinishedTaskCompletionSource = new TaskCompletionSource<bool>();
+            await UnityMainThreadDispatcher.Instance().EnqueueAsync(dispatcher =>
+                MoveAction(dispatcher, tokenPositionMarkerGameObjectName, squareGameObjectName));
+            await _coroutineFinishedTaskCompletionSource.Task;
         }
-        var moveTask = UnityMainThreadDispatcher.Instance().EnqueueAsync(MoveAction);
+
         _gameBoard.CurrentSquare = square;
-        
-        return moveTask;
+    }
+    
+    private void MoveAction(MonoBehaviour dispatcher, string positionMarkerGameObjectName, string destinationSquareGameObjectName)
+    {
+        var destinationSquareTransform = GetGameBoardGameObject().GetComponentsInChildren<Transform>().Single(c => c.gameObject.name == destinationSquareGameObjectName);
+        var destinationPosition = destinationSquareTransform
+            .GetComponentsInChildren<Transform>().Single(c => c.gameObject.name == positionMarkerGameObjectName).position;
+        dispatcher.StartCoroutine(ProgressivelyMoveTowards(destinationPosition, 5f));
     }
 
-    private GameObject GetPlayerTokenGameObject()
+    private IEnumerator ProgressivelyMoveTowards(Vector3 destinationPosition, float speed)
+    {
+        var playerPosition = GetPlayerTokenGameObject().transform.position;
+        while (playerPosition != destinationPosition)
+        {
+            playerPosition = IsPlayerPositionAlmostDestination(destinationPosition, playerPosition)
+                ? destinationPosition
+                : Vector3.MoveTowards(playerPosition, destinationPosition, speed * Time.deltaTime);
+            
+            var playerGameObject = GetPlayerTokenGameObject();
+            playerGameObject.transform.position = playerPosition;
+            yield return null;
+        }
+        _coroutineFinishedTaskCompletionSource.SetResult(true);
+        yield return null;
+    }
+
+    private static bool IsPlayerPositionAlmostDestination(Vector3 destinationPosition, Vector3 playerPosition)
+    {
+        return Vector3.Magnitude(playerPosition - destinationPosition) < .1;
+    }
+
+    private static GameObject GetPlayerTokenGameObject()
     {
         return GameObject.Find("Player");
     }
